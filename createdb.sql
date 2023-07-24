f190ab58bbae2a0e3d4f938e4c5a5c650694fd41
gansbodom <gansbodom@gmail.com>
2023-07-25 01:55:34 +0500

    fkrewofwekf

--- createdb.sql
+++ createdb.sql
@@ -1,53 +1,47 @@
-CREATE DATABASE music_shop;
-
-\connect music_shop
-
-SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL SERIALIZABLE;
-
-create table if not exist "Artist"(
+create table if not exists "Artist"(
     "artist_id" serial primary key,
     "artist_name" varchar(80) not null
 );
 
-create table if not exist "Genre"(
+create table if not exists "Genre"(
     "genre_id" serial primary key,
     "genre_name" varchar(80) not null
 );
 
-create table if not exist "Artist_genre"(
+create table if not exists "Artist_genre"(
     "artist_id" integer not null references Artist(artist_id),
     "genre_id" integer not null references Genre(genre_id)
 constraint pk primary key(artist_id, genre_id),
 );
 
-create table if not exist "Albums"(
+create table if not exists "Albums"(
     "album_id" serial primary key,
     "album_name" varchar(80) not null,
-year integer not null,
+year integer not null
 );
 
-create table "Album_artist"(
-    "artist_id" integer not null references Artist(artist_id),
-    "album_id" integer not null references Album(album_id)
-constraint pk primary key(artist_id, album_id),
+create table if not exists "Album_artist"(
+    "artist_id" integer not null references "Artist" ("id"),
+    "album_id" integer not null references "Albums" ("album_id"),
+constraint pk primary key("artist_id", "album_id")
 );
 
-create table if not exist "Tracks"(
+create table if not exists "Tracks"(
     "track_id" serial primary key,
     "track_name" varchar(80) not null,
-    "album_id" integer not null references Albums(album_id),
+    "album_id" integer not null references "Albums" ("album_id"),
     "duration" time not null
 );
 
-create table "Compilations"(
+create table if not exists "Compilations"(
     "compilation_id" serial primary key,
-    "name" varchrar(80) not null,
-year integer not null,
+    "name" varchar(80) not null,
+year integer not null
 );
 
-create table "Track_compilation"(
-    "track_id" integer not null references Tracks(track_id),
-    "compilation_id" integer not null references Compilations(compilation_id)
-constraint pk primary key (track_id, compilation_id),
+create table  if not exists "Track_compilation"(
+    "track_id" integer not null references "Tracks" ("track_id"),
+    "compilation_id" integer not null references "Compilations" ("compilation_id"),
+constraint pk primary key ("track_id", "compilation_id")
 );
 
