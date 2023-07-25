d0422f4bca2331d5c0be2760ed229db818818243
gansbodom <gansbodom@gmail.com>
2023-07-25 15:33:58 +0500

    final update

--- select.sql
+++ select.sql
@@ -16,4 +16,11 @@ SELECT album_id
 FROM public."Albums"
 where "year" between 2018 and 2020;
 
+SELECT artist_name
+FROM public."Artist"
+WHERE not artist_name like '% %';
+
+SELECT track_name 
+FROM public."Tracks"
+WHERE track_name like "Мой%";
 
